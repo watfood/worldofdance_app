f5fd771402e1c664b9f192d36ef557323c79d0ae
Duane Than <eventsco@aol.com>
2016-02-25 16:43:19 -0800

    updated User Index/View, DNCR logo

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
  validates :first_name, presence: true
  validates :last_name, presence: true
 
- has_attached_file :profile_img_url, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "http://scienceblogs.com/startswithabang/files/2008/05/t0m537.jpg"
+ has_attached_file :profile_img_url, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "https://i.imgur.com/6GKm4Ld.jpg"
  validates_attachment_content_type :profile_img_url, content_type: /\Aimage\/.*\Z/
 
 
